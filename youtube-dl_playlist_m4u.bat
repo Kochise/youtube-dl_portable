fb37aea8cd68c48cd5daded5bbf9198fbab91a7c
Kochise <david.koch@9online.fr>
2020-03-16 20:12:01 +0100

    Add raw m4u loader, add keep option for mp3 loader

---
+++ youtube-dl_playlist_m4u.bat
@@ -0,0 +1,4 @@
+youtube-dl --ignore-errors --continue --hls-prefer-native --no-overwrites --no-progress --no-mark-watched --no-call-home --all-subs --sub-format "srt/best" -f "bestaudio[ext=m4a]" --write-thumbnail --write-description --batch-file youtube-dl_playlist.txt --yes-playlist 1>youtube-dl_stdout.txt 2>youtube-dl_stderr.txt
+
+rem --write-info-json --write-annotations
+rem --simulate --get-id --get-description --get-duration --get-filename --get-format
---
+++ youtube-dl_videos_mp3_forced.bat
@@ -0,0 +1,4 @@
+youtube-dl --ignore-errors --continue --hls-prefer-native --no-overwrites --no-progress --no-mark-watched --no-call-home --all-subs --sub-format "srt/best" -f "bestaudio[ext=m4a]" --extract-audio --audio-format mp3 --audio-quality 0 --write-thumbnail --write-description --batch-file youtube-dl_videos.txt -k 1>youtube-dl_stdout.txt 2>youtube-dl_stderr.txt
+
+rem --write-info-json --write-annotations
+rem --simulate --get-id --get-description --get-duration --get-filename --get-format
