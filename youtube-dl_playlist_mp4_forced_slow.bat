5f1798b4ebd6a686816deecc90efe6d6b4d47beb
Kochise <david.koch@9online.fr>
2020-05-22 15:53:09 +0200

    Add more options

---
+++ youtube-dl_playlist_mp4_forced_slow.bat
@@ -0,0 +1,4 @@
+youtube-dl --ignore-errors --continue --hls-prefer-native --no-overwrites --no-progress --no-mark-watched --no-call-home --all-subs --sub-format "srt/best" -f "bestvideo[height<=1080][fps<50][ext=mp4]+bestaudio[ext=m4a]/mp4" --merge-output-format mp4 --write-description --batch-file youtube-dl_playlist.txt --yes-playlist --limit-rate 300K 1>youtube-dl_stdout.txt 2>youtube-dl_stderr.txt
+
+rem --write-info-json --write-annotations
+rem --simulate --get-id --get-description --get-duration --get-filename --get-format
