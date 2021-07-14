3c0095bad2772a79363e4a83ea879fd1fe1267ff
Kochise <david.koch@9online.fr>
2021-07-14 22:09:18 +0200

    Update youtube-dl

---
+++ ffmpeg_crop.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal enableExtensions disableDelayedExpansion
+
+set /p twidth="Width (px): "
+set /p theight="Height (px): "
+set /p tleft="Left (px): "
+set /p ttop="Top (px): "
+
+rem ffmpeg -i "%~1" -filter:v "crop=%twidth%:%theight%:%tleft%:%ttop%" -vcodec copy -acodec copy "%~dpn1-crop%~x1"
+ffmpeg -i "%~1" -filter:v "crop=%twidth%:%theight%:%tleft%:%ttop%" "%~dpn1-crop%~x1"
+rem ffmpeg -i "%~1" -c copy -bsf:v h264_metadata=crop_left=%tleft%:crop_right=%tleft%:crop_top=%ttop%:crop_bottom=%ttop% "%~dpn1-crop%~x1"
--- ffmpeg_vertical_aspect_ratio.bat
+++ ffmpeg_vertical_aspect_ratio.bat
@@ -1,3 +1,7 @@
 @echo off
+rem https://trac.ffmpeg.org/wiki/Scaling
 
-ffmpeg -i "%~1" -aspect 720:1280 -c copy "%~dpn1_vertical%~x1"
+rem ffmpeg -i "%~1" -aspect 720:1280 -c copy "%~dpn1_vertical%~x1"
+rem ffmpeg -i "%~1" -aspect 720:1280 -filter:v scale=iw:-2 -c:a copy "%~dpn1_vertical%~x1"
+rem ffmpeg -i "%~1" -aspect 720:1280 -filter:v scale=320:568 -c:a copy "%~dpn1_vertical%~x1"
+ffmpeg -i "%~1" -aspect 720:1280 -filter:v scale=480:-2 -c:a copy "%~dpn1_vertical%~x1"
