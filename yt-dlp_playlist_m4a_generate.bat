be232c7bf61d7ef59a854b9f3713760e6e7b5b9c
Kochise <david.koch@9online.fr>
2022-10-17 12:16:43 +0200

    Update helpers (playlist generator)

--- yt-dlp_playlist_m4a_generate.bat
+++ yt-dlp_playlist_m4a_generate.bat
@@ -1,1 +1,18 @@
-dir /o:n /b *.m4a>%~n0.m3u
+@echo off && setlocal enabledelayedexpansion && chcp 65001>nul
+
+rem Current path (beware if 'root')
+set "PATH_FULL=%~p0"
+
+:loop
+for /f "tokens=1* delims=\" %%a in ("%PATH_FULL%") do (
+	set "PATH_LAST=%%a"
+	set "PATH_FULL=%%b"
+)
+rem Loop as long as we're not at the end
+if defined PATH_FULL goto :loop
+
+rem If no last folder found (ie 'root') use batch's name
+if not defined PATH_LAST set "PATH_LAST=%~n0"
+
+rem Create playlist based on name (should be checked with source though)
+dir /o:n /b *.m4a>"%PATH_LAST%.m3u"
